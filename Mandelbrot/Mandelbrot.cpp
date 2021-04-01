f790d4443d077c1193fd55bffb0d28d0fe9ecada
HJanuszewski <76430960+HJanuszewski@users.noreply.github.com>
2021-04-01 12:41:11 +0200

    Lab example, non-parallel cpu version of mandelbrot implemented

--- Mandelbrot/Mandelbrot.cpp
+++ Mandelbrot/Mandelbrot.cpp
@@ -1,2 +1,34 @@
 ﻿// Using mandelbrot set example by Adam Sampson <a.sampson@abertay.ac.uk> as the base for this class
 #include "Mandelbrot.h"
+
+void Mandelbrot::generate(double left, double right, double top, double bot) // regular, non-parallelized version of the funcion, running on the CPU
+{
+
+	for (int y = 0; y < height; y++) // those loops should be parallelized, as they go over every pixel, one at a time
+	{
+		for (int x = 0; x < width; x++)
+		{
+			std::complex<double> c(left + (x * (right - left) / width), top + (y * (bot - top) / height));
+			std::complex<double> z(0.0, 0.0);
+			int i = 0;
+			
+			while (abs(z) < 2.0 && i < iterations)
+			{
+				z = (z * z) + c;
+
+				++i;
+			}
+
+			if (i == iterations)
+			{
+				Mandelbrot::image[y][x] = 0x000000; //black
+			}
+			else
+			{
+				Mandelbrot::image[y][x] = 0xFFFFFF; //while
+			}
+		}
+
+
+	}
+}
--- Mandelbrot/Mandelbrot.h
+++ Mandelbrot/Mandelbrot.h
@@ -1,8 +1,9 @@
 #pragma once
 #include <cstdint>
+#include <complex>
 #define height 1440
 #define width 3440
-#define iterations 500;
+constexpr int iterations = 500; // At first I wanted to use #define for all 3, but compiler was not happy with this one being used in the while loop for some reason.
 // Using mandelbrot set example by Adam Sampson <a.sampson@abertay.ac.uk> as the base for this class
 class Mandelbrot
 {
