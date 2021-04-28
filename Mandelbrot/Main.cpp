0c434ecbb487373f54d011dad66e4d959ee29b37
HJanuszewski <76430960+HJanuszewski@users.noreply.github.com>
2021-04-28 15:16:14 +0200

    borked it

--- Mandelbrot/Main.cpp
+++ Mandelbrot/Main.cpp
@@ -1,6 +1,6 @@
 #include "Mandelbrot.h"
 
- void write_tga(const char* name, std::uint32_t img[height][width]) // Same write function as the lab example, with very minor changes
+ void write_tga(const char* name, std::atomic<std::uint32_t> img[height][width]) // Same write function as the lab example, with very minor changes
 {
 	std::ofstream outfile(name, std::ofstream::binary);
 
@@ -43,59 +43,7 @@
 }
 
 
- void write_tga_thread(const char* name, bool atomic, Mandelbrot* obj)
-{
-	std::ofstream outfile(name, std::ofstream::binary);
-	
-	
-	uint8_t header[18] = {
-		0, // no image ID
-		0, // no colour map
-		2, // uncompressed 24-bit image
-		0, 0, 0, 0, 0, // empty colour map specification
-		0, 0, // X origin
-		0, 0, // Y origin
-		width & 0xFF, (width >> 8) & 0xFF, // width
-		height & 0xFF, (height >> 8) & 0xFF, // height
-		24, // bits per pixel
-		0, // image descriptor
-	};
-	outfile.write((const char*)header, 18);
-	
-		
-		for (int y = 0; y < height; y++)
-		{
-			std::unique_lock<std::mutex> ul(obj->line_mutex[y]);
-			obj->write_condition[y].wait(ul, [&] { if (obj->line_completed[y]) return true; else return false; });
-			//block until the line with the ID of y is completed
-			if (atomic)
-			{
-				for (int x = 0; x < width; x++)
-				{
-					uint8_t pixel[3] = {
-						 obj->image_atomic[y][x] & 0xFF, // blue channel
-						(obj->image_atomic[y][x] >> 8) & 0xFF, // green channel
-						(obj->image_atomic[y][x] >> 16) & 0xFF, // red channel
-					};
-					outfile.write((const char*)pixel, 3);
-				}
-			}
-			else
-			{
-				for (int x = 0; x < width; x++)
-				{
-					uint8_t pixel[3] = {
-						 obj->image[y][x] & 0xFF, // blue channel
-						(obj->image[y][x] >> 8) & 0xFF, // green channel
-						(obj->image[y][x] >> 16) & 0xFF, // red channel
-					};
-					outfile.write((const char*)pixel, 3);
-				}
-			}
-		}
-	
-	
-}
+
 
 
 
@@ -107,7 +55,7 @@ int main()
 	int threads;
 	bool atomic,colour,manual_threads;
 	double args[4];
-	const char filename[15] = "Mandelbrot.tga";
+	
 	uint32_t bg_colour = 0xFFFFFF;
 	uint32_t fg_colour = 0x000000;
 
@@ -153,7 +101,8 @@ int main()
 
 
 	Mandelbrot* image = new Mandelbrot;
-	
+	image->atomic = atomic;
+	tbb::task_arena write(1);
 
 	switch (selection)
 	{
@@ -162,7 +111,7 @@ int main()
 		
 		std::chrono::steady_clock::time_point start = std::chrono::steady_clock::now(); //start the timer
 		image->generate_original(args, bg_colour, fg_colour); // generate the set using original, non-parallel code.
-		write_tga(filename,image->image);
+		//write_tga(filename,image->image);
 		std::chrono::steady_clock::time_point stop = std::chrono::steady_clock::now();
 		std::cout << "It took " << std::chrono::duration_cast<std::chrono::milliseconds>(stop - start).count() << " ms" << std::endl;
 		
@@ -171,9 +120,9 @@ int main()
 	case 211: // parallel for - atomic variable - thread limit 
 	{
 		std::chrono::steady_clock::time_point start = std::chrono::steady_clock::now(); // Start the clock
-		std::thread write(write_tga_thread, filename, atomic, image); // Start the file-writing thread
+		//std::thread write(write_tga_thread, filename, atomic, image); // Start the file-writing thread
 		image->generate_parallel_for(args, image->image_atomic, bg_colour, fg_colour,threads);
-		write.join(); //Wait for the file-writing thread to finish. File-writing thread will only finish after all lines are completed.
+		//write.join(); //Wait for the file-writing thread to finish. File-writing thread will only finish after all lines are completed.
 		std::chrono::steady_clock::time_point stop = std::chrono::steady_clock::now(); // Stop the clock
 		std::cout << "It took " << std::chrono::duration_cast<std::chrono::milliseconds>(stop - start).count() << " ms" << std::endl; // Display the time it took to generate and write the set to a file.
 		break;
@@ -181,9 +130,13 @@ int main()
 	case 210: // parallel for - atomic variable - no thread limit
 	{
 		std::chrono::steady_clock::time_point start = std::chrono::steady_clock::now(); // Start the clock
-		std::thread write(write_tga_thread, filename, atomic, image); // Start the file-writing thread
+		//std::thread write(write_tga_thread, filename, atomic, image); // Start the file-writing thread
 		image->generate_parallel_for(args, image->image_atomic, bg_colour, fg_colour);
-		write.join(); //Wait for the file-writing thread to finish. File-writing thread will only finish after all lines are completed.
+		write.execute([&] {
+			//write_tga_thread(filename,atomic,image);
+			});
+		//write.join(); //Wait for the file-writing thread to finish. File-writing thread will only finish after all lines are completed.
+		//write_tga(filename,image->image_atomic);
 		std::chrono::steady_clock::time_point stop = std::chrono::steady_clock::now(); // Stop the clock
 		std::cout << "It took " << std::chrono::duration_cast<std::chrono::milliseconds>(stop - start).count() << " ms" << std::endl; // Display the time it took to generate and write the set to a file.
 		break;
@@ -191,9 +144,9 @@ int main()
 	case 201: // parallel for - unique_lock - thread limit
 	{
 		std::chrono::steady_clock::time_point start = std::chrono::steady_clock::now(); // Start the clock
-		std::thread write(write_tga_thread, filename, atomic, image); // Start the file-writing thread
+		//std::thread write(write_tga_thread, filename, atomic, image); // Start the file-writing thread
 		image->generate_parallel_for(args, image->image, bg_colour, fg_colour,threads);
-		write.join(); //Wait for the file-writing thread to finish. File-writing thread will only finish after all lines are completed.
+		//write.join(); //Wait for the file-writing thread to finish. File-writing thread will only finish after all lines are completed.
 		std::chrono::steady_clock::time_point stop = std::chrono::steady_clock::now(); // Stop the clock
 		std::cout << "It took " << std::chrono::duration_cast<std::chrono::milliseconds>(stop - start).count() << " ms" << std::endl; // Display the time it took to generate and write the set to a file.
 		break;
@@ -201,9 +154,9 @@ int main()
 	case 200: // parallel for - unique_lock - no thread limit
 	{
 		std::chrono::steady_clock::time_point start = std::chrono::steady_clock::now(); // Start the clock
-		std::thread write(write_tga_thread, filename, atomic, image); // Start the file-writing thread
+		//std::thread write(write_tga_thread, filename, atomic, image); // Start the file-writing thread
 		image->generate_parallel_for(args, image->image, bg_colour, fg_colour);
-		write.join(); //Wait for the file-writing thread to finish. File-writing thread will only finish after all lines are completed.
+		//write.join(); //Wait for the file-writing thread to finish. File-writing thread will only finish after all lines are completed.
 		std::chrono::steady_clock::time_point stop = std::chrono::steady_clock::now(); // Stop the clock
 		std::cout << "It took " << std::chrono::duration_cast<std::chrono::milliseconds>(stop - start).count() << " ms" << std::endl; // Display the time it took to generate and write the set to a file.
 		break;
@@ -211,9 +164,9 @@ int main()
 	case 311: // nested parallel for - atomic variable - thread limit
 	{
 		std::chrono::steady_clock::time_point start = std::chrono::steady_clock::now(); // Start the clock
-		std::thread write(write_tga_thread, filename, atomic, image); // Start the file-writing thread
+		//std::thread write(write_tga_thread, filename, atomic, image); // Start the file-writing thread
 		image->generate_nested_parallel_for(args, image->image_atomic, bg_colour, fg_colour,threads);
-		write.join(); //Wait for the file-writing thread to finish. File-writing thread will only finish after all lines are completed.
+		//write.join(); //Wait for the file-writing thread to finish. File-writing thread will only finish after all lines are completed.
 		std::chrono::steady_clock::time_point stop = std::chrono::steady_clock::now(); // Stop the clock
 		std::cout << "It took " << std::chrono::duration_cast<std::chrono::milliseconds>(stop - start).count() << " ms" << std::endl; // Display the time it took to generate and write the set to a file.
 		break;
@@ -222,9 +175,9 @@ int main()
 	{
 		
 		std::chrono::steady_clock::time_point start = std::chrono::steady_clock::now(); // Start the clock
-		std::thread write(write_tga_thread, filename, atomic, image); // Start the file-writing thread
+		//std::thread write(write_tga_thread, filename, atomic, image); // Start the file-writing thread
 		image->generate_nested_parallel_for(args, image->image_atomic, bg_colour, fg_colour);
-		write.join(); //Wait for the file-writing thread to finish. File-writing thread will only finish after all lines are completed.
+	//	write.join(); //Wait for the file-writing thread to finish. File-writing thread will only finish after all lines are completed.
 		std::chrono::steady_clock::time_point stop = std::chrono::steady_clock::now(); // Stop the clock
 		std::cout << "It took " << std::chrono::duration_cast<std::chrono::milliseconds>(stop - start).count() << " ms" << std::endl; // Display the time it took to generate and write the set to a file.
 		break;
@@ -233,9 +186,9 @@ int main()
 	case 301: // nested parallel for - unique_lock - thread limit
 	{
 		std::chrono::steady_clock::time_point start = std::chrono::steady_clock::now(); // Start the clock
-		std::thread write(write_tga_thread, filename, atomic, image); // Start the file-writing thread
+	//	std::thread write(write_tga_thread, filename, atomic, image); // Start the file-writing thread
 		image->generate_nested_parallel_for(args, image->image, bg_colour, fg_colour, threads);
-		write.join(); //Wait for the file-writing thread to finish. File-writing thread will only finish after all lines are completed.
+		//write.join(); //Wait for the file-writing thread to finish. File-writing thread will only finish after all lines are completed.
 		std::chrono::steady_clock::time_point stop = std::chrono::steady_clock::now(); // Stop the clock
 		std::cout << "It took " << std::chrono::duration_cast<std::chrono::milliseconds>(stop - start).count() << " ms" << std::endl; // Display the time it took to generate and write the set to a file.
 		break;
@@ -245,9 +198,9 @@ int main()
 	case 300: // nested parallel for - unique_lock - no thread limit
 	{
 		std::chrono::steady_clock::time_point start = std::chrono::steady_clock::now(); // Start the clock
-		std::thread write(write_tga_thread, filename, atomic, image); // Start the file-writing thread
+	//	std::thread write(write_tga_thread, filename, atomic, image); // Start the file-writing thread
 		image->generate_nested_parallel_for(args, image->image, bg_colour, fg_colour);
-		write.join(); //Wait for the file-writing thread to finish. File-writing thread will only finish after all lines are completed.
+	//	write.join(); //Wait for the file-writing thread to finish. File-writing thread will only finish after all lines are completed.
 		std::chrono::steady_clock::time_point stop = std::chrono::steady_clock::now(); // Stop the clock
 		std::cout << "It took " << std::chrono::duration_cast<std::chrono::milliseconds>(stop - start).count() << " ms" << std::endl; // Display the time it took to generate and write the set to a file.
 		break;
@@ -256,9 +209,9 @@ int main()
 	case 411:
 	{
 		std::chrono::steady_clock::time_point start = std::chrono::steady_clock::now(); // Start the clock
-		std::thread write(write_tga_thread, filename, atomic, image); // Start the file-writing thread
+	//	std::thread write(write_tga_thread, filename, atomic, image); // Start the file-writing thread
 		image->generate_nested_parallel_for_func(args, image->image_atomic, bg_colour, fg_colour,threads);
-		write.join(); //Wait for the file-writing thread to finish. File-writing thread will only finish after all lines are completed.
+	//	write.join(); //Wait for the file-writing thread to finish. File-writing thread will only finish after all lines are completed.
 		std::chrono::steady_clock::time_point stop = std::chrono::steady_clock::now(); // Stop the clock
 		std::cout << "It took " << std::chrono::duration_cast<std::chrono::milliseconds>(stop - start).count() << " ms" << std::endl; // Display the time it took to generate and write the set to a file.
 		break;
@@ -268,9 +221,9 @@ int main()
 	case 410:
 	{
 		std::chrono::steady_clock::time_point start = std::chrono::steady_clock::now(); // Start the clock
-		std::thread write(write_tga_thread, filename, atomic, image); // Start the file-writing thread
+	//	std::thread write(write_tga_thread, filename, atomic, image); // Start the file-writing thread
 		image->generate_nested_parallel_for_func(args, image->image_atomic, bg_colour, fg_colour);
-		write.join(); //Wait for the file-writing thread to finish. File-writing thread will only finish after all lines are completed.
+	//	write.join(); //Wait for the file-writing thread to finish. File-writing thread will only finish after all lines are completed.
 		std::chrono::steady_clock::time_point stop = std::chrono::steady_clock::now(); // Stop the clock
 		std::cout << "It took " << std::chrono::duration_cast<std::chrono::milliseconds>(stop - start).count() << " ms" << std::endl; // Display the time it took to generate and write the set to a file.
 		break;
@@ -280,9 +233,9 @@ int main()
 	case 401:
 	{
 		std::chrono::steady_clock::time_point start = std::chrono::steady_clock::now(); // Start the clock
-		std::thread write(write_tga_thread, filename, atomic, image); // Start the file-writing thread
+	//	std::thread write(write_tga_thread, filename, atomic, image); // Start the file-writing thread
 		image->generate_nested_parallel_for_func(args, image->image, bg_colour, fg_colour, threads);
-		write.join(); //Wait for the file-writing thread to finish. File-writing thread will only finish after all lines are completed.
+	//	write.join(); //Wait for the file-writing thread to finish. File-writing thread will only finish after all lines are completed.
 		std::chrono::steady_clock::time_point stop = std::chrono::steady_clock::now(); // Stop the clock
 		std::cout << "It took " << std::chrono::duration_cast<std::chrono::milliseconds>(stop - start).count() << " ms" << std::endl; // Display the time it took to generate and write the set to a file.
 		break;
@@ -292,9 +245,9 @@ int main()
 	case 400:
 	{
 		std::chrono::steady_clock::time_point start = std::chrono::steady_clock::now(); // Start the clock
-		std::thread write(write_tga_thread, filename, atomic, image); // Start the file-writing thread
+	//	std::thread write(write_tga_thread, filename, atomic, image); // Start the file-writing thread
 		image->generate_nested_parallel_for_func(args, image->image, bg_colour, fg_colour);
-		write.join(); //Wait for the file-writing thread to finish. File-writing thread will only finish after all lines are completed.
+		//write.join(); //Wait for the file-writing thread to finish. File-writing thread will only finish after all lines are completed.
 		std::chrono::steady_clock::time_point stop = std::chrono::steady_clock::now(); // Stop the clock
 		std::cout << "It took " << std::chrono::duration_cast<std::chrono::milliseconds>(stop - start).count() << " ms" << std::endl; // Display the time it took to generate and write the set to a file.
 		break;
@@ -307,7 +260,7 @@ int main()
 
 
 	while (1)
-	static int x = std::getchar();
+	std::getchar();
 	//std::chrono::steady_clock::time_point start = std::chrono::steady_clock::now(); // start point of the timing
 	//std::chrono::steady_clock::time_point stop = std::chrono::steady_clock::now(); // stop point of the timing.
 	//image->write_tga("output.tga", image->image);
--- Mandelbrot/Mandelbrot.h
+++ Mandelbrot/Mandelbrot.h
@@ -16,17 +16,21 @@ class Mandelbrot
 private:
 	
 	bool compute_single_pixel(std::complex<double> c); // A piece of the original function, taking an already calculated c value and returning true/false depending on if that point exists in the set.
+	const char filename[15] = "Mandelbrot.tga";
 	
 public:
+	
 	uint32_t image[height][width];
 	std::atomic<uint32_t> image_atomic[height][width];
 	std::mutex line_mutex[height];
 	std::condition_variable write_condition[height]; // create a condition variable for each line that will
 	bool line_completed[height] = { false }; //initialize the entire array to be false, since no line has been generated yet.
+	bool atomic;
 
 	void generate_original(double values[4], uint32_t bg_colour, uint32_t fg_colour); // The original function that was used in the lab example for generating the set, not parallelised at all.
 	
-	
+	void write_tga_thread(const char* name, bool atomic);
+
 	template<typename T> void generate_parallel_for(double values[4], T (&img)[height][width], uint32_t bg_colour, uint32_t fg_colour );
 	template<typename T> void generate_parallel_for(double values[4], T(&img)[height][width], uint32_t bg_colour, uint32_t fg_colour, int threads); 
 	
@@ -99,15 +103,75 @@ bool Mandelbrot::compute_single_pixel(std::complex<double> c)
 
 // ---------- PARALLEL FUNCTIONS -----------
 
+
+void Mandelbrot::write_tga_thread(const char* name, bool atomic)
+{
+	std::ofstream outfile(name, std::ofstream::binary);
+
+
+	uint8_t header[18] = {
+		0, // no image ID
+		0, // no colour map
+		2, // uncompressed 24-bit image
+		0, 0, 0, 0, 0, // empty colour map specification
+		0, 0, // X origin
+		0, 0, // Y origin
+		width & 0xFF, (width >> 8) & 0xFF, // width
+		height & 0xFF, (height >> 8) & 0xFF, // height
+		24, // bits per pixel
+		0, // image descriptor
+	};
+	outfile.write((const char*)header, 18);
+
+
+	for (int y = 0; y < height; y++)
+	{
+		std::unique_lock<std::mutex> ul(Mandelbrot::line_mutex[y]);
+		Mandelbrot::write_condition[y].wait(ul, [&] { if (Mandelbrot::line_completed[y]) return true; else return false; });
+		//block until the line with the ID of y is completed
+		if (atomic)
+		{
+			for (int x = 0; x < width; x++)
+			{
+				uint8_t pixel[3] = {
+					 Mandelbrot::image_atomic[y][x] & 0xFF, // blue channel
+					(Mandelbrot::image_atomic[y][x] >> 8) & 0xFF, // green channel
+					(Mandelbrot::image_atomic[y][x] >> 16) & 0xFF, // red channel
+				};
+				outfile.write((const char*)pixel, 3);
+			}
+		}
+		else
+		{
+			for (int x = 0; x < width; x++)
+			{
+				uint8_t pixel[3] = {
+					 Mandelbrot::image[y][x] & 0xFF, // blue channel
+					(Mandelbrot::image[y][x] >> 8) & 0xFF, // green channel
+					(Mandelbrot::image[y][x] >> 16) & 0xFF, // red channel
+				};
+				outfile.write((const char*)pixel, 3);
+			}
+		}
+	}
+
+
+}
+
+
+
 template<typename T>void Mandelbrot::generate_parallel_for(double values[4], T(&img)[height][width], uint32_t bg_colour, uint32_t fg_colour)
 {
 
 	std::mutex image_mut;
+	tbb::task_arena write(1);
+	write.execute([&] {Mandelbrot::write_tga_thread(Mandelbrot::filename, Mandelbrot::atomic); });
 	tbb::parallel_for(0, height, [&](int i) {
 
+		std::lock_guard<std::mutex> lg(line_mutex[i]);
 		for (int x = 0; x < width; x++)
 		{
-			std::lock_guard<std::mutex> lg(line_mutex[i]);
+			
 			std::complex<double> c(values[0] + (x * (values[1] - values[0]) / width), values[2] + (i * (values[3] - values[2]) / height));
 			std::complex<double> z(0.0, 0.0);
 			int it = 0;
@@ -144,14 +208,15 @@ template<typename T>void Mandelbrot::generate_parallel_for(double values[4], T(&
 template<typename T>void Mandelbrot::generate_parallel_for(double values[4], T(&img)[height][width], uint32_t bg_colour, uint32_t fg_colour, int threads)
 {
 
-	std::mutex image_mut;  // Mutex used to safely share the 'img' array
+	//std::mutex image_mut;  // Mutex used to safely share the 'img' array
 	tbb::task_arena thread_limit(threads); // Task arena which is needed by TBB to limit the amount of threads that will run
 	thread_limit.execute([&] { // Running the generation code inside a lambda expression in the 'thread_limit' task arena, to limit the thread number to the 'threads' value
 		tbb::parallel_for(0, height, [&](int i) { // Parallel for from TBB will run a separate thread for each 'i' value, however starting no more threads than the limit that was imposed by task arena.
 
+			std::lock_guard<std::mutex> lg(line_mutex[i]);
 			for (int x = 0; x < width; x++)
 			{
-				std::lock_guard<std::mutex> lg(line_mutex[i]);
+				
 				std::complex<double> c(values[0] + (x * (values[1] - values[0]) / width), values[2] + (i * (values[3] - values[2]) / height));
 				std::complex<double> z(0.0, 0.0);
 				int it = 0;
@@ -165,13 +230,13 @@ template<typename T>void Mandelbrot::generate_parallel_for(double values[4], T(&
 
 				if (it == iterations)
 				{
-					std::unique_lock<std::mutex> lock(image_mut);
+					//std::unique_lock<std::mutex> lock(image_mut);
 					img[i][x] = fg_colour;
 
 				}
 				else
 				{
-					std::unique_lock<std::mutex> lock(image_mut);
+					//std::unique_lock<std::mutex> lock(image_mut);
 					img[i][x] = bg_colour;
 				}
 			}
@@ -188,11 +253,13 @@ template<typename T>void Mandelbrot::generate_parallel_for(double values[4], T(&
 template<typename T> void Mandelbrot::generate_nested_parallel_for(double values[4], T(&img)[height][width], uint32_t bg_colour, uint32_t fg_colour)
 {
 
-	std::mutex image_mut;
+	//std::mutex image_mut;
 	tbb::parallel_for(0, height, [&](int i) {
 
+		std::lock_guard<std::mutex> lg(line_mutex[i]);
 		tbb::parallel_for(0, width, [&](int j) {
 
+			
 			std::complex<double> c(values[0] + (j * (values[1] - values[0]) / width), values[2] + (i * (values[3] - values[2]) / height));
 			std::complex<double> z(0.0, 0.0);
 			int it = 0;
@@ -206,18 +273,20 @@ template<typename T> void Mandelbrot::generate_nested_parallel_for(double values
 
 			if (it == iterations)
 			{
-				std::unique_lock<std::mutex> lock(image_mut);
+				//std::unique_lock<std::mutex> lock(image_mut);
 				img[i][j] = fg_colour;
 
 			}
 			else
 			{
-				std::unique_lock<std::mutex> lock(image_mut);
+				//std::unique_lock<std::mutex> lock(image_mut);
 				img[i][j] = bg_colour;
 
 			}
-			});
 		});
+		//Mandelbrot::line_completed[i] = true;
+		Mandelbrot::write_condition[i].notify_one();
+	});
 
 
 
@@ -231,8 +300,10 @@ template<typename T> void Mandelbrot::generate_nested_parallel_for(double values
 	thread_limit.execute([&] {
 		tbb::parallel_for(0, height, [&](int i) {
 
+			std::lock_guard<std::mutex> lg(line_mutex[i]);
 			tbb::parallel_for(0, width, [&](int j) {
 
+				
 				std::complex<double> c(values[0] + (j * (values[1] - values[0]) / width), values[2] + (i * (values[3] - values[2]) / height));
 				std::complex<double> z(0.0, 0.0);
 				int it = 0;
@@ -257,7 +328,10 @@ template<typename T> void Mandelbrot::generate_nested_parallel_for(double values
 
 				}
 				});
+			Mandelbrot::line_completed[i] = true;
+			Mandelbrot::write_condition[i].notify_one();
 			});
+		
 		});
 
 
@@ -272,8 +346,10 @@ template<typename T> void Mandelbrot::generate_nested_parallel_for_func(double v
 	std::mutex image_mut;
 	tbb::parallel_for(0, height, [&](int i) {
 
+		std::lock_guard<std::mutex> lg(line_mutex[i]);
 		tbb::parallel_for(0, width, [&](int j) {
 
+			
 			if (Mandelbrot::compute_single_pixel((values[0] + (j * (values[1] - values[0]) / width), values[2] + (i * (values[3] - values[2]) / height))))
 			{
 				std::unique_lock<std::mutex> lock(image_mut);
@@ -286,8 +362,10 @@ template<typename T> void Mandelbrot::generate_nested_parallel_for_func(double v
 				img[i][j] = bg_colour;
 
 			}
-			});
 		});
+		Mandelbrot::line_completed[i] = true;
+		Mandelbrot::write_condition[i].notify_one();
+	});
 
 
 
@@ -301,9 +379,9 @@ template<typename T> void Mandelbrot::generate_nested_parallel_for_func(double v
 	thread_limit.execute([&] {
 
 		tbb::parallel_for(0, height, [&](int i) {
-
+			std::lock_guard<std::mutex> lg(line_mutex[i]);
 			tbb::parallel_for(0, width, [&](int j) {
-
+				
 				if (Mandelbrot::compute_single_pixel((values[0] + (j * (values[1] - values[0]) / width), values[2] + (i * (values[3] - values[2]) / height))))
 				{
 					std::unique_lock<std::mutex> lock(image_mut);
@@ -317,8 +395,10 @@ template<typename T> void Mandelbrot::generate_nested_parallel_for_func(double v
 
 				}
 				});
+			Mandelbrot::line_completed[i] = true;
+			Mandelbrot::write_condition[i].notify_one();
 			});
-
+		
 		});
 
 
